edb27f069c5d4744de1b59d993c13d7d927898a5
Chirag Patel <chiragpatel499@gmail.com>
2025-12-26 10:28:43 +0530

    allowed host all

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9ct#xnnvp*#09#*=4g+h@j&tf-m!$-l5911(zm_1v7@bn=w1#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
